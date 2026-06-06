406008a4230ee7b187601be0001633b5b1cffcfe
juyanlanfeng <3557677639@qq.com>
2026-06-06 08:29:32 +0800

    合并后整理

--- include/Order_mgr.hpp
+++ include/Order_mgr.hpp
@@ -6,23 +6,20 @@
 
 class OrderService{
 public:
-    OrderService() : order_(), order_history_(), total_price_(0.0) {}
+    OrderService(const User& user) : user_(user), order_(), order_history_(), total_price_(0.0) {}
     virtual ~OrderService() = default;
 
-    virtual void addDish(const Dish& dish) = 0;
-    virtual void removeDish(const Dish& dish) = 0;
-    virtual double calcCart() = 0;
-    virtual double checkout() = 0;
-    virtual double getDiscountRate(const User& user_) = 0;
-    virtual void updateUser(const User& user_, const std::vector<Dish>& order_, double total_price_) = 0;
-    virtual static std::vector<std::vector<std::string>> loadUserHistoryOrders(const User& user_, const std::string& history_path) = 0;
+    virtual void addDish(const Dish& dish);
+    virtual void removeDish(const Dish& dish);
+    virtual double calcCart();
+    virtual double checkout();
+    virtual double getDiscountRate(const User& user_);
+    virtual void updateUser(const User& user_, const std::vector<Dish>& order_, double total_price_);
+    static std::vector<std::vector<Dish>> loadUserHistoryOrders(const User& user_, const std::string& history_path);
 
 private:
+    User user_;
     std::vector<Dish> order_;
     std::vector<std::vector<Dish>> order_history_;
-
     double total_price_;
 };
-
-
-// calcCart(), checkout(), getDiscountRate(), cloneOrder(), addToHistory()
