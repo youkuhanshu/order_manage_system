dab362a59649195bfddfc558e8fec49f9ff2164a
youkuhanshu <2035697131@qq.com>
2026-06-14 00:07:06 +0800

    更新readme，修复重复随机计数问题

--- ui/src/comment_dialog.h
+++ ui/src/comment_dialog.h
@@ -5,6 +5,7 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QFrame>
+#include <QComboBox>
 #include "FileManager.h"
 
 class CommentDialog : public QDialog
@@ -14,8 +15,12 @@ class CommentDialog : public QDialog
 public:
     explicit CommentDialog(const Dish_qt &dish, QWidget *parent);
 
+private slots:
+    void onSortChanged(int index);
+
 private:
     void setupUI(const Dish_qt &dish, const QList<CommentMsg> &comments);
+    void refreshCommentList();
     QWidget *makeCommentCard(const CommentMsg &comment, QWidget *parent);
     QFrame  *makeRatingBar(int star, int count, int total, QWidget *parent);
     QString matchIDName(std::string id);
@@ -23,4 +28,13 @@ private:
     static QString formatTime(std::time_t t);
     static QString starStr(int rate);
     static std::vector<User> m_users;
+
+    // 排序与数据
+    QList<CommentMsg> m_comments;     // 当前显示的所有评论
+    int m_sortMode = 0;               // 0=按时间, 1=按评分
+    QComboBox *m_sortCombo = nullptr;
+
+    // 评论列表容器（排序切换时重建）
+    QVBoxLayout *m_listLayout = nullptr;
+    QWidget     *m_listContainer = nullptr;
 };
